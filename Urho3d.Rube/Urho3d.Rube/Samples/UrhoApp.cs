ded7333ad043fad3be04ab80a9135cdc7c57b0c0
dgzornoza <dgzornoza@dgzornoza.com>
2018-06-22 18:38:06 +0200

    -- corregidos samples con la nueva toolkit

--- Urho3d.Rube/Urho3d.Rube/Samples/UrhoApp.cs
+++ Urho3d.Rube/Urho3d.Rube/Samples/UrhoApp.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Text;
 using Urho;
 using Urho.Urho2D;
+using XamarinForms.Toolkit.Urho3D.Rube;
 
 namespace Urho3d.Rube.Samples
 {
@@ -54,8 +55,8 @@ namespace Urho3d.Rube.Samples
 
             // TEMP: load rube
             string filePath = ResourceCache.GetResourceFileName("Urho2D/RubePhysics/documentA.json");
-            Toolkit.Urho.Rube.B2dJson b2dJson = new Toolkit.Urho.Rube.B2dJson();
-            b2dJson.ReadIntoNodeFromFile(filePath, this._scene.CreateChild("physicsNode"), out string errorMsg);
+            B2dJson b2dJson = new B2dJson();
+            b2dJson.ReadIntoNodeFromFile(filePath, this._scene.CreateChild("physicsNode"), true, out string errorMsg);
         }
 
 
